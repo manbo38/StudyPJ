3fd67238316ca914fd8840869d057966ac553dc7
manbo38 <ziastar@naver.com>
2024-08-13 18:14:40 +0900

    Add files via upload

---
+++ Main_5.java
@@ -0,0 +1,16 @@
+package p2024_08_06;
+
+import java.util.Scanner;
+
+public class Main5 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		for(int i=1; i<10; i++) {
+				System.out.println(N + " * " + i + " = "+ (N * i));
+			}
+	}
+
+}
