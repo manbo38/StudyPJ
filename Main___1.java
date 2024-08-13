f5edd555c696b83c71cc16858fe32f90cdbb865d
manbo38 <ziastar@naver.com>
2024-08-13 18:16:26 +0900

    Add files via upload

---
+++ Main___1.java
@@ -0,0 +1,24 @@
+package p2024_08_08;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		  
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			if(a==0 && b==0) {
+				break;
+			}
+			int sum = a + b;
+			System.out.println(sum);
+		}
+		
+	}
+
+}
