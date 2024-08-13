fe6578b54955cff97e9f195d2089efd463be2194
manbo38 <ziastar@naver.com>
2024-08-13 20:05:09 +0900

    [Bronze III] Title: 최댓값, Time: 208 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] a = new int[9];
+		
+		for(int i=0; i<a.length; i++) {
+			a[i] = sc.nextInt();
+		}
+		
+		int max = a[0];
+		int maxIndex = 0;
+		for(int i=1; i<a.length; i++) {
+			if(max < a[i]) {
+				max = a[i];
+				maxIndex = i;
+			}
+		}System.out.println(max);
+		System.out.println(maxIndex+1);
+		
+	}
+
+}
