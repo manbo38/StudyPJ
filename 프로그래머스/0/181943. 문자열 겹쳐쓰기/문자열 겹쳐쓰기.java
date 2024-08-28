cf51fbb8760165beb4566fa43bdb21619edd4eaa
manbo38 <ziastar@naver.com>
2024-08-28 17:30:53 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 23.12 ms, Memory: 70.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+         String result = my_string.substring(0, s) 
+                      + overwrite_string 
+                      + my_string.substring(s + overwrite_string.length());
+        return result;
+        
+    }
+}
