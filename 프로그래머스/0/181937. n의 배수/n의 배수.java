7362521b4fa55fc81103b428646ca74625820ca5
manbo38 <ziastar@naver.com>
2024-08-29 18:05:43 +0900

    [level 0] Title: n의 배수, Time: 0.04 ms, Memory: 69.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181937. n의 배수/n의 배수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num, int n) {
+        int answer = 1;
+        int answer2 = 0;
+        
+        if(num%n == 0) {
+            return answer;
+        }else{
+            return answer2;
+        }
+        
+    }
+}
