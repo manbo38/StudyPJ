c28d18eef6ff1cfe77d7c47ccb038d7c174c1575
manbo38 <ziastar@naver.com>
2024-08-29 18:18:47 +0900

    [level 0] Title: 공배수, Time: 0.03 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int number, int n, int m) {
+        int answer = 1;
+        int answer2 = 0;
+        
+        if(number%n == 0 && number%m == 0) {
+            return answer;
+        }else{
+            return answer2;
+        }
+    }
+}
