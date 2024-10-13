2537dfa1a089b7cc55db8da874fa89831f9ca243
manbo38 <ziastar@naver.com>
2024-10-13 21:37:58 +0900

    [level 0] Title: 나이 출력, Time: 0.04 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int age) {
+        int answer = 0;
+        
+        int year = 2024;
+        answer = year - age - 1;
+        
+        return answer;
+    }
+}
